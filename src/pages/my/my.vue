cdba3d089f0c0fc7cc09a5f48f325691fbe34834
boxbears <2293212443@qq.com>
2023-12-30 15:47:56 +0800

    Merge remote-tracking branch 'origin/dev' into dev

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,25 +1,49 @@
 <template>
+  <!-- 自定义导航栏 -->
+  <CNavbar />
   <div class="container">
     <Head class="head"></Head>
     <Surf class="surf"></Surf>
-    <pButton>购物车</pButton>
-    <pButton>我的订单</pButton>
-    <pButton>隐私设置</pButton>
-    <pButton>商家注册</pButton>
+    <pButton @tap="ShoppingCart">购物车</pButton>
+    <pButton @tap="MyOrder">我的订单</pButton>
+    <pButton @tap="MyPrivacy">隐私设置</pButton>
+    <pButton @tap="Business">商家注册</pButton>
     <button><div class="word">退出登录</div></button>
   </div>
 </template>
 
 <script setup lang="ts">
+import CNavbar from './components/CNavbar.vue'
 import Surf from './components/surf.vue'
 import Head from './components/head.vue'
 import pButton from './components/pButton.vue'
+
+const ShoppingCart = () => {
+  uni.navigateTo({
+    url: '/pages/my/components/ShoppingCart',
+  })
+}
+const MyOrder = () => {
+  uni.navigateTo({
+    url: '/pages/my/components/MyOrder',
+  })
+}
+const MyPrivacy = () => {
+  uni.navigateTo({
+    url: '/pages/my/components/MyPrivacy',
+  })
+}
+const Business = () => {
+  uni.navigateTo({
+    url: '/pages/my/components/Business',
+  })
+}
 </script>
 
 <style lang="scss" scoped>
 .container {
-  background-color: rgb(224, 224, 224);
-  height: 1500rpx;
+  background-color: #f7f7f8;
+  height: 100%;
   // display: flex;
   // flex-direction: column;
   button {
---
+++ src/services/login.ts
@@ -0,0 +1,16 @@
+import type { LoginResult } from '@/types/member'
+import { http } from '@/utils/http'
+
+/**
+ * 小程序登录_内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
